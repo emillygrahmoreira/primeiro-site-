ee9f74743dedf1146699b8f8e58b2a7a478bd262
emillygrahmoreira <emilly.grah.moreira@escola.pr.gov.br>
2026-06-11 09:04:08 -0300

    fiz botão de tamanho, de contraste

---
+++ script.js
@@ -0,0 +1,18 @@
+const botao = document.querySelector("button")
+    botao.addEventListener('click', curtir)
+
+    function curtir (){
+       let curtidas = document.querySelector("span")
+       curtidas.textContent++;
+    }
+
+
+    //BOTÃO DO TAMANHO
+    const tamanho = document.getElementById("tamanho")
+    tamanho.addEventListener("click", aumentarFonte)
+    let fonte = 16;
+
+    //FUNÇÃO
+    function aumentarFonte(){
+        document.body.style.fontStyle = fonte + "px";
+    }
